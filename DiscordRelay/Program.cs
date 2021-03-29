980fae6a1ed22202741ffa7c129b45b85f3e75c1
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-03-29 19:05:13 +0100

    Create Program.cs

---
+++ DiscordRelay/Program.cs
@@ -0,0 +1,48 @@
+﻿using DiscordRelay.Discord;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Info: startup");
+            DiscordBot mybot = new DiscordBot();
+            mybot.setToken(Environment.GetEnvironmentVariable("Discord_Token"));
+            if(ulong.TryParse(Environment.GetEnvironmentVariable("Discord_Target_server"),out ulong targetserverid) == true)
+            {
+                if (ulong.TryParse(Environment.GetEnvironmentVariable("Discord_Target_channel"), out ulong targetchannelid) == true)
+                {
+                    mybot.setTargetServer(targetserverid, targetchannelid);
+                }
+            }
+            if (ulong.TryParse(Environment.GetEnvironmentVariable("Discord_Master"), out ulong targetmasterid) == true)
+            {
+                mybot.setMaster(targetmasterid);
+            }
+            int loop = 1;
+            bool exit = false;
+            while(exit == false)
+            {
+                exit = true;
+                if (ulong.TryParse(Environment.GetEnvironmentVariable("Relay_"+loop.ToString()+"_server"), out ulong relayserverid) == true)
+                {
+                    if (ulong.TryParse(Environment.GetEnvironmentVariable("Relay_" + loop.ToString() + "_channel"), out ulong relaychannelid) == true)
+                    {
+                        mybot.addRelayServer(relayserverid, relaychannelid);
+                        exit = false;
+                    }
+                }
+                loop++;
+            }
+            mybot.StartRelay();
+            while(mybot.exit() == false)
+            {
+                Thread.Sleep(5000);
+            }
+            Console.WriteLine("Info: shutting down");
+        }
+    }
+}
